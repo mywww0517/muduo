483a4ec620fd07eb48a0abf2a0867c2683a30a9f
mywww <3505707283@qq.com>
2026-03-09 11:34:37 +0800

    实现最小化框架，建立build文件夹

---
+++ src/client/main.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+int main() {
+  std::cout << "chatclient start\n";
+  return 0;
+}
---
+++ src/server/main.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+int main() {
+  std::cout << "chatserver start\n";
+  return 0;
+}
